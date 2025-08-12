06748d85931163c0d2eb2595a92602ed51d4bc8c
LucasHernandezzz <lucas.hr021@outlook.com>
2025-08-12 20:08:32 -0300

    initial commit

---
+++ src/test/kotlin/com/epi/app/EpiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epi.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EpiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
